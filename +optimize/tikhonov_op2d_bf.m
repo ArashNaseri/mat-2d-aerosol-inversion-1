5b45f80938ed6e0c707722412393e2ab8cee3c74
ArashNaseri <61205412+ArashNaseri@users.noreply.github.com>
2020-03-08 00:25:31 -0700

    Add files via upload
    The skim of 2D optimization is modified.

--- +optimize/tikhonov_op2d_bf.m
+++ +optimize/tikhonov_op2d_bf.m
@@ -32,16 +32,16 @@ for ii=length(param):-1:1 % reverse loop to pre-allocate
     
     %-- Perform inversion --%
     [out(ii).x,~,Lpr0] = invert.tikhonov(...
-        [param(ii,2).*A;C],[param(ii,2).*b;d],param(ii,1),Lpr0,[],xi,solver);
+        [A;C],[param(ii,2)*b;d],param(ii,1),Lpr0,[],xi,solver);
     %-- Store ||Ax-b|| and Euclidean error --%
     if ~isempty(x_ex); out(ii).chi = norm(out(ii).x-x_ex); end
      out(ii).Axb = norm(A*out(ii).x-b);
     
     %-- Compute credence, fit, and Bayes factor --%
-    out(ii).x = invert.tikhonov([param(ii,2)*A;C],[param(ii,2)*b;d],...
+    out(ii).x = invert.tikhonov([A;C],[param(ii,2)*b;d],...
         param(ii,1),Lpr0,[],xi,solver);
     [out(ii).B,out(ii).F,out(ii).C] = ...
-        optimize.bayesf_b([param(ii,2)*A;C],[param(ii,2)*b;d],...
+        optimize.bayesf_b([A;C],[param(ii,2)*b;d],...
         out(ii).x,Lpr0,param(ii,1));
     tools.textbar((length(param)-ii+1)/length(param));
 end
@@ -54,12 +54,12 @@ out(1).ind_min = ind_min;
 
 %-- Add fminsearch step to optimize parameter set --------------------%
 disp('Optimizing Tikhonov regularization:');
-fun = @(lambda) log(-1*optimize.bayesf_b([lambda(2)*A;C],[lambda(2)*b;d],invert.tikhonov...
-    ([lambda(2)*A;C],[lambda(2)*b;d],lambda(1),Lpr0,[],xi,solver),Lpr0,lambda(1)));
+fun = @(lambda) -optimize.bayesf_b([A;C],[lambda(2)*b;d],invert.tikhonov...
+    ([A;C],[lambda(2)*b;d],lambda(1),Lpr0,[],xi,solver),Lpr0,lambda(1));
 
 y0 = [out(ind_min).lambda out(ind_min).alpha]; % initial guess for fminsearch
-options = optimset('TolFun',10^-8,'TolX',10^-8,'Display','iter');
-y1 = fminsearch(fun,y0,options); % get optimal lambda and alpha
+options = optimset('MaxFunEvals',300,'TolFun',10^-4,'TolX',10^-4,'PlotFcns',@optimplotfval,'Display','iter');
+y1 = fminsearch(fun,y0,options); %get optimal lambda and alpha
 
 lambda = y1(1); % assign output variables
 alpha = y1(2);
@@ -68,8 +68,7 @@ disp('Complete.');
 
 
 x = invert.tikhonov(...
-    [alpha*A;C],[alpha*b;d],lambda,Lpr0,[],xi,solver);
-chi = norm(x-x_ex);
-
+    [A;C],[alpha*b;d],lambda,Lpr0,[],xi,solver);
+if and(exist('x_ex','var'),norm(x_ex)~=0);chi = norm(x-x_ex);else chi = []; end
 
 end
